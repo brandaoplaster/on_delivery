44023e7d028ba023590b68a9c613b23399225358
brandaoplaster <brandaoplaster@gmail.com>
2021-04-15 23:30:56 -0300

    chore: [WIP] agent user

---
+++ lib/users/agent.ex
@@ -0,0 +1,11 @@
+defmodule OnDelivery.Users.Agent do
+  alias OnDelivery.Users.User
+
+  use Agent
+
+  def start_link(_initial_state) do
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
+  end
+
+  def save(%User{} = user), do: Agent.update(__MODULE__)
+end

b6e6aca75270b36996cd9527b640ba0a862e609c
brandaoplaster <brandaoplaster@gmail.com>
2021-04-18 18:12:22 -0300

    feat: Adding delegate for orders

--- lib/on_delivery.ex
+++ lib/on_delivery.ex
@@ -1,10 +1,14 @@
 defmodule OnDelivery do
-  alias OnDelivery.Users.CreateOrUpdate
   alias OnDelivery.Users.Agent, as: UserAgent
+  alias OnDelivery.Orders.Agent, as: OrderAgent
+  alias OnDelivery.Users.CreateOrUpdate, as: CreateOrUpdateUser
+  alias OnDelivery.Orders.CreateOrUpdate, as: CreateOrUpdateOrder
 
   def start_agentes do
     UserAgent.start_link(%{})
+    OrderAgent.start_link(%{})
   end
 
-  defdelegate create_or_update_user(params), to: CreateOrUpdate, as: :call
+  defdelegate create_or_update_user(params), to: CreateOrUpdateUser, as: :call
+  defdelegate create_or_update_order(params), to: CreateOrUpdateOrder, as: :call
 end
